4050cabb40469566eaf33621b4a0388db406ab63
chococake22 <fightest21@gmail.com>
2022-03-08 23:54:23 +0900

    Spring Security 적용 및 로그인 & 로그아웃 구현

---
+++ src/main/java/janghowon/terminal/config/SecurityConfig.java
@@ -0,0 +1,43 @@
+package janghowon.terminal.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    // 보안 필터 적용이 필요없는 리소스 설정
+    // css, js, image image 등은 필요가 없다.
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/resources/**");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/", "/notice", "/write/{id}", "/location", "/board/search", "/searchtime").permitAll()
+                .antMatchers("/time/timeadd").hasRole("USER")
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .usernameParameter("apple")
+                .passwordParameter("1234")
+                .defaultSuccessUrl("/")
+                .permitAll()
+                .and()
+                .logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/")
+                .invalidateHttpSession(true)
+                .and()
+                .exceptionHandling()
+                .accessDeniedPage("/error");
+
+    }
+}
