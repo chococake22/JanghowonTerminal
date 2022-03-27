886f24e353c5257dff3811043de0ba066d1185b2
chococake22 <fightest21@gmail.com>
2022-03-27 23:51:49 +0900

    회원가입 테스트 코드 작성

---
+++ src/test/java/janghowon/terminal/controller/AccountControllerTest.java
@@ -0,0 +1,56 @@
+package janghowon.terminal.controller;
+
+import janghowon.terminal.domain.Account;
+import janghowon.terminal.dto.AccountDto;
+import janghowon.terminal.repository.AccountBusInfoRepository;
+import janghowon.terminal.repository.AccountRepository;
+import janghowon.terminal.role.Role;
+import janghowon.terminal.service.AccountService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.ArrayList;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class AccountControllerTest {
+
+    @Autowired
+    private AccountService accountService;
+
+    @Autowired
+    private AccountRepository accountRepository;
+
+    @Autowired
+    private AccountBusInfoRepository accountBusInfoRepository;
+
+    @Test
+    void 회원가입() {
+
+        // given
+        Account account = new Account(5L, "test1", "test12#$A", "test1@test.com", "01022224555", Role.USER);
+        AccountDto accountDto = AccountDto.builder()
+                .id(account.getId())
+                .username(account.getUsername())
+                .password(account.getPassword())
+                .email(account.getEmail())
+                .phone(account.getPhone())
+                .role(account.getRole())
+                .build();
+
+        accountService.save(accountDto);
+
+        // when
+        AccountDto accountDto1 = accountService.getAccount(accountDto.getUsername());
+
+        // then
+        Assertions.assertThat(accountDto1.getUsername()).isEqualTo(account.getUsername());
+    }
+
+
+
+}
--- src/test/java/janghowon/terminal/controller/BoardControllerTest.java
+++ src/test/java/janghowon/terminal/controller/BoardControllerTest.java
@@ -1,6 +1,5 @@
 package janghowon.terminal.controller;
 
-import janghowon.terminal.auth.AccountDetails;
 import janghowon.terminal.domain.Account;
 import janghowon.terminal.domain.Board;
 import janghowon.terminal.dto.BoardDto;
@@ -16,8 +15,6 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.ArrayList;
 import java.util.Optional;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
@@ -36,7 +33,7 @@ public class BoardControllerTest {
     void 글작성() {
         // given
         Account a = new Account(1L, "haha", "1234", "haha@naver.com", "01012341234", Role.USER);
-        Board b = new Board(20L, a, "title1", "content1", new ArrayList<>());
+        Board b = new Board(21L, a, "title1", "content1", new ArrayList<>());
         BoardDto boardDto = BoardDto.builder()
                 .id(b.getId())
                 .account(b.getAccount())
@@ -69,38 +66,23 @@ public class BoardControllerTest {
         Assertions.assertThat(selected.get().getTitle()).isEqualTo(boardDto4.getTitle());
     }
 
-//    @Test
-//    void 글수정() {
-//        // given
-//        Board b = new Board(1L, "apple", "title", "이거는 내용입니다.");
-//        BoardDto boardDto = BoardDto.builder()
-//                .id(b.getId())
-//                .writer(b.getWriter())
-//                .title(b.getTitle())
-//                .content(b.getContent())
-//                .createdDate(b.getCreatedDate())
-//                .modifiedDate(b.getModifiedDate())
-//                .build();
-//
-//        boardService.save(boardDto);
-//
-//        // when
-//        Optional<Board> board = boardRepository.findById(b.getId());
-//        BoardDto boardDto1 = BoardDto.builder()
-//                .id(board.get().getId())
-//                .writer(board.get().getWriter())
-//                .title(board.get().getTitle())
-//                .content(board.get().getContent())
-//                .createdDate(board.get().getCreatedDate())
-//                .modifiedDate(board.get().getModifiedDate())
-//                .build();
-//        boardDto1.setWriter("banana");
-//        boardController.update(boardDto1);
-//
-//        // then
-//        BoardDto boardDto11 = boardService.getBoard(b.getId());
-//        assertThat(boardDto11.getWriter()).isEqualTo("banana");
-//
-//
-//    }
+    @Test
+    void 글수정() {
+
+        // when
+        Account a = new Account(1L, "haha", "1234", "haha@naver.com", "01012341234", Role.USER);
+        Board b = new Board(21L, a, "title1", "content1", new ArrayList<>());
+        BoardDto boardDto = BoardDto.builder()
+                .id(b.getId())
+                .account(b.getAccount())
+                .title(b.getTitle())
+                .content(b.getContent())
+                .comments(b.getComments())
+                .build();
+
+        // given
+
+
+
+    }
 }
