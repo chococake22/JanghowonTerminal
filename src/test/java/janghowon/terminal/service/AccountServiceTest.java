39e3c92d46616d8444d9eb7449157bc03d20d823
chococake22 <fightest21@gmail.com>
2022-03-30 20:53:24 +0900

    드디어 mock, InjectMocks의 사용법을 알게 되었다

--- src/test/java/janghowon/terminal/service/AccountServiceTest.java
+++ src/test/java/janghowon/terminal/service/AccountServiceTest.java
@@ -4,6 +4,7 @@ import janghowon.terminal.domain.Account;
 import janghowon.terminal.dto.AccountDto;
 import janghowon.terminal.repository.AccountRepository;
 import janghowon.terminal.role.Role;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -26,16 +27,17 @@ import static org.mockito.Mockito.*;
 @Transactional
 public class AccountServiceTest {
 
-    @Mock    // Mock 객체가 주입될 클래스(주입 받을 객체)
-    private AccountService accountService;
-
     @Mock   // 주입할 객체
     private AccountRepository accountRepository;
 
     @Mock
     BCryptPasswordEncoder bCryptPasswordEncoder;
 
+    @InjectMocks    // Mock 객체가 주입될 클래스(주입 받을 객체)
+    private AccountService accountService;
+
     static AccountDto accountDto = new AccountDto(2L, "haha", "1234","haha@haha.com", "01022443355", Role.USER);
+    static Account account = new Account(3L, "test1", "1234", "test1@test.com", "01054315432", Role.USER);
 
     @Test
     void mockRepository() {
@@ -43,9 +45,12 @@ public class AccountServiceTest {
         // 아무거나 넣어도 accountDto를 반환하게 메서드 설정
         // accountRepository의 메서드를 정의
         // interface의 메서드도 설정이 가능하다.
-        when(accountRepository.findByUsername(any())).thenReturn(Optional.of(accountDto.toEntity()));
+        when(accountRepository.findByUsername(any())).thenReturn(Optional.of(account));
 
-        assertThat(accountRepository.findByUsername(accountDto.getUsername()).get().getUsername()).isEqualTo(accountDto.getUsername());
+        assertThat(accountService.getAccount(account.getUsername()).getPhone()).isEqualTo(account.getPhone());
+
+
+
     }
 
     @Test
